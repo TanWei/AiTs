cbff368074fd6f8fb762b8bbea3c1c2ce1179e11
lei89 <dcsvgwudi@gmail.com>
2022-01-13 01:05:23 +0800

    头文件

---
+++ practice/resnet_paddle.py
@@ -0,0 +1,17 @@
+import paddle.vision.transforms as T
+
+#加载飞桨和相关类库
+import paddle
+from paddle.nn import Linear
+import paddle.nn.functional as F
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Residual():
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    paddleversion = paddle.__version__
+    print("paddle version:", paddleversion)
