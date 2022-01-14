12845685337c9e1181afaebb9f88c1351753b80c
lei89 <dcsvgwudi@gmail.com>
2022-01-14 22:40:47 +0800

    houseprice_paddle

--- practice/resnet_paddle.py
+++ practice/resnet_paddle.py
@@ -3,13 +3,14 @@
 #加载飞桨和相关类库
 import paddle
 from paddle.nn import Linear
+import paddle.nn as nn
 import paddle.nn.functional as F
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 from paddle.vision.models import resnet50
 
-class Residual():
+class Residual(nn.Layer):
     def __init__(self):
         pass
 
