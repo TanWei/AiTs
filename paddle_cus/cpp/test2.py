8a31fc66da4727b4ec2983e9f25b37c608f20d45
lei89 <dcsvgwudi@gmail.com>
2022-08-17 00:42:45 +0800

    自定义算子使用全路径保证编译成功

--- paddle_cus/cpp/test2.py
+++ paddle_cus/cpp/test2.py
@@ -5,7 +5,7 @@ paddle.device.set_device('cpu')
 
 custom_ops = load(
     name="custom_jit_ops",
-    sources=["relu_cpu_fp32.cc"])
+    sources=[r"/Users/leilei/MyWorkSpace/ml/AiTs/paddle_cus/cpp/relu_cpu_fp32.cc"])
 
 x = paddle.randn([4, 10], dtype='float32')
 out = custom_ops.custom_relu(x)
